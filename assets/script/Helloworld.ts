a087616ebe7966af5566bf87443508dec7a66ed3
xiaocongcc <237786026@qq.com>
2019-06-20 15:24:05 +0800

    添加script、scene、texture

---
+++ assets/script/Helloworld.ts
@@ -0,0 +1,39 @@
+import ProtoLoader from "./protobuf/ProtoLoader";
+import { NetWork } from "./framework/net/NetWork";
+import { Login } from "./ProtoMessage";
+
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class Helloworld extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    start() {
+        // init logic
+        this.label.string = this.text;
+
+        let cfgman = require('CfgMan');
+        console.log(cfgman[1].name);  // 小明
+
+        ProtoLoader.load(() => {
+            let network = new NetWork();
+            let url = 'ws://localhost:3000';
+            network.connect(url);
+            setTimeout(() => {
+                network.send('hello');
+
+                let login = new Login();
+                login.cmd = 'login';
+                login.name = 'Clever';
+                login.pw = '123456';
+                network.send(login);
+            }, 1000);
+        });
+    }
+}
